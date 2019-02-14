524a73c4801c47ef5448b5ecc97f998ce21a2162
Victoracha <36788662+Victoracha@users.noreply.github.com>
2019-02-14 01:46:47 +0100

    Ultimos retoques

--- ReglasDePrioridad/src/app/api.service.ts
+++ ReglasDePrioridad/src/app/api.service.ts
@@ -12,8 +12,8 @@ const httpOptions = {
 })
 
 export class APIService {
-  API_URL ='http://'+window.location.hostname+':8000';
-  //API_URL = 'http://localhost:8000';
+  //API_URL ='http://'+window.location.hostname+':8000';
+  API_URL = 'http://localhost:8000';
   constructor(private httpClient: HttpClient) { }
   getPieza() {
 
